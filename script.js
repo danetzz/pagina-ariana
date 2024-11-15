a454162eb470af8cbffce8d870a34840c31f7756
danetzz <basiledanila@gmail.com>
2024-11-15 17:23:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -255,16 +255,5 @@ document.getElementById('searchButton').addEventListener('click', async function
     }
   });
 
-
- // RESPONSIVE MENÚ HAMBURGUESA
-  // Obtenemos los elementos
-const hamburger = document.getElementById('hamburgesa');
-const menuHamburguesa = document.getElementById('listadoHamburguesa');
-
-// Añadimos un evento al botón hamburguesa
-hamburger.addEventListener('click', () => {
-    menuHamburguesa.style.display= "block"; // Alternamos la clase 'show' para abrir/cerrar el menú
-});
-
   
 
